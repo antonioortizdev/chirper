89e652618a5ff34f1b8ee0d54525c1e8aaa6d9eb
Antonio Ortiz <antonioortizdev@gmail.com>
2023-01-21 18:20:43 +0100

    fix linter and formatter

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,17 +1,28 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ['next', 'turbo', 'prettier'],
+  plugins: ['unused-imports'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-    "semi": ["error", "never"],
-    "sort-imports": [
-        "error",
-        {
-            "ignoreCase": true,
-            "ignoreDeclarationSort": true,
-            "memberSyntaxSortOrder": ["none", "all", "multiple", "single"]
-        }
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    semi: ['error', 'never'],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
     ],
-    "comma-style": ["error", "last"],
+    'comma-style': ['error', 'last'],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 }
