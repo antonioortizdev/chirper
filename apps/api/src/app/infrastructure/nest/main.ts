0045c47c43df2e7f1e800588eb4e5a0f1e349d70
Antonio Ortiz <antonioortizdev@gmail.com>
2023-01-28 11:21:39 +0100

    fix env variable call

--- apps/api/src/app/infrastructure/nest/main.ts
+++ apps/api/src/app/infrastructure/nest/main.ts
@@ -6,7 +6,7 @@ import { config as dotenvConfig } from 'dotenv'
 dotenvConfig()
 
 const bootstrap = async () => {
-  const port = process.env.PORT || 3000
+  const port = process.env.API_PORT || 3000
   const app = await NestFactory.create(AppModule)
   await app.listen(port)
   console.log(`🚀 Server listening to http://localhost:${port}`)
