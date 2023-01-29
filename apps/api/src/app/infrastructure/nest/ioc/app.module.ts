34a0d2ffc73090c68fe42b9405c9ea6bd9df2a37
Antonio Ortiz <antonioortizdev@gmail.com>
2023-01-29 12:13:06 +0100

    fix: module trying to use unconfigured typeorm

--- apps/api/src/app/infrastructure/nest/ioc/app.module.ts
+++ apps/api/src/app/infrastructure/nest/ioc/app.module.ts
@@ -1,21 +1,10 @@
 import { ChirpModule } from '../../../../chirp/infrastructure/ioc/ChirpModule'
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { config as dotenvConfig } from 'dotenv'
 
 dotenvConfig()
 
-const typeOrmModule = TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: Number(process.env.MYSQL_PORT) || 3306,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASS || 'root',
-  database: process.env.MYSQL_DATABASE || 'chirper',
-  synchronize: process.env.APP_ENV !== 'production',
-})
-
 @Module({
-  imports: [ChirpModule, typeOrmModule],
+  imports: [ChirpModule],
 })
 export class AppModule {}
