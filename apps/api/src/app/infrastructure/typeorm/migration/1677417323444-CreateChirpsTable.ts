5109cb03e09647b0d4bdd78c17fdbd81fe0a617e
Antonio Ortiz <antonioortizdev@gmail.com>
2023-02-26 15:33:36 +0100

    chore: remove dummy chirps insert,create table either it exists or not

--- apps/api/src/app/infrastructure/typeorm/migration/1677417323444-CreateChirpsTable.ts
+++ apps/api/src/app/infrastructure/typeorm/migration/1677417323444-CreateChirpsTable.ts
@@ -1,9 +1,7 @@
-import { ChirpTypeOrmEntity } from '../../../../chirp/infrastructure/persistence/typeorm/entity/ChirpTypeOrmEntity'
 import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class CreateChirpsTable1677417323444 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const ifNotExists = true
     await queryRunner.createTable(
       new Table({
         name: 'chirps',
@@ -23,23 +21,7 @@ export class CreateChirpsTable1677417323444 implements MigrationInterface {
           },
         ],
       }),
-      ifNotExists,
     )
-
-    const chirps: ChirpTypeOrmEntity[] = [
-      {
-        id: '9069e1e9-88b1-4a32-a334-3dff680c0981',
-        author: 'John',
-        message: 'Hello world!',
-      },
-      {
-        id: 'dc4ea3ca-95ce-4d40-a34d-b9c6b6733187',
-        author: 'Jane',
-        message: 'How are you doing?',
-      },
-    ]
-
-    await queryRunner.manager.save(ChirpTypeOrmEntity, chirps)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
